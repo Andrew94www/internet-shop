786605bdf273291a93c84dcff97de080666aaa02
Andrew Popelnukha <popelnucha70@gmail.com>
2022-09-25 19:49:06 +0300

    day 4

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+
 class MainController extends Controller
 {
     public function index() {
@@ -20,7 +21,15 @@ class MainController extends Controller
         return view('category', compact('category'));
     }
 
-    public function product($product = null) {
+    public function product($category, $product = null) {
         return view('product', ['product' => $product]);
+    }
+
+    public function basket() {
+        return view('basket');
+    }
+
+    public function basketPlace() {
+        return view('order');
     }
 }
